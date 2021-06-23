d4aeb435007205bfd801a377a78ddaf2185c80f8
Nycole <nycole.xavierr@gmail.com>
2021-06-22 21:01:21 -0300

    finish second class

--- main.js
+++ main.js
@@ -18,3 +18,17 @@ for (const link of links) {
         nav.classList.remove('show')
     })
 }
+
+/* Mudar o header da página quando der o scroll*/
+const header = document.querySelector("#header")
+const navHeight = header.offsetHeight
+
+window.addEventListener("scroll", function() {
+    if(window.scrollY >= navHeight) {
+        // scroll é maior que a altura do header
+        header.classList.add('scroll')
+    } else {
+        // menor que a altura do header
+        header.classList.remove('scroll')
+    }
+})
